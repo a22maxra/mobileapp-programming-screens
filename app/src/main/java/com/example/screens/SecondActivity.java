28cc8af4dd0d7425659017cc6bf79fb24b25b08a
a22maxra <max.rapp01@gmail.com>
2023-04-26 13:03:15 +0200

    Display data

---
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -0,0 +1,22 @@
+package com.example.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        TextView textView = findViewById(R.id.displayData);
+        Intent intent = getIntent();
+        String text = intent.getStringExtra("text");
+        textView.setText(text);
+
+    }
+}
